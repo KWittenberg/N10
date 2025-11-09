5fcd6558e71f6d31ecc349dbacb41f1e30252562
Krešimir Wittenberg <kresimir.wittenberg@gmail.com>
2025-11-09 08:21:49 +0100

    update

---
+++ Enums/ItemsPerPage.cs
@@ -0,0 +1,22 @@
+﻿namespace N10.Enums;
+
+public enum ItemsPerPage
+{
+    [Display(Name = "25 po stranici")]
+    TwentyFive = 25,
+
+    [Display(Name = "50 po stranici")]
+    Fifty = 50,
+
+    [Display(Name = "75 po stranici")]
+    SeventyFive = 75,
+
+    [Display(Name = "100 po stranici")]
+    OneHundred = 100,
+
+    [Display(Name = "125 po stranici")]
+    OneHundredTwentyFive = 125,
+
+    [Display(Name = "150 po stranici")]
+    OneHundredFifty = 150
+}
