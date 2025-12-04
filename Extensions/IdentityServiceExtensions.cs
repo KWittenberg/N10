0ea7fb1e7e1cf91a213c14660a626563f8ea8e8a
Krešimir Wittenberg <kresimir.wittenberg@gmail.com>
2025-12-04 03:09:11 +0100

    update

--- Extensions/IdentityServiceExtensions.cs
+++ Extensions/IdentityServiceExtensions.cs
@@ -19,45 +19,20 @@ public static class IdentityServiceExtensions
 
 
         // ✅ Add database
-        //var connectionIndex = 3; // Set: [0]Development [1]Remote [2]Production [3]PostgreSQL [4]MySQL
-        //var connectionName = new[] { "Development", "Remote", "Production", "PostgreSQL", "MySQL" };
-        //var connectionString = configuration.GetConnectionString(connectionName[connectionIndex]) ?? throw new InvalidOperationException("Connection not found!");
-
-        //services.AddDbContextFactory<ApplicationDbContext>(options =>
-        //{
-        //    if (connectionIndex == 3) options.UseNpgsql(connectionString);
-        //    // else if (connectionIndex == 4) options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-        //    else options.UseSqlServer(connectionString);
-        //}, ServiceLifetime.Scoped);
-
-
-        // ✅ Add database
         var index = configuration.GetValue<int>("Database:Index", 0);
         var names = configuration.GetSection("Database:Names").Get<string[]>() ?? throw new InvalidOperationException("Missing database names!");
         var cs = configuration.GetConnectionString(names[index]) ?? throw new InvalidOperationException("Connection not found!");
 
         services.AddDbContextFactory<ApplicationDbContext>(options =>
         {
-            // if (index == 3) options.UseNpgsql(cs);
-            // else if (index == 4) options.UseMySql(cs, ServerVersion.AutoDetect(cs));
+            // if (index == 3 || index == 4) options.UseNpgsql(cs);
+            // else if (index == 5) options.UseMySql(cs, ServerVersion.AutoDetect(cs));
             // else options.UseSqlServer(cs);
             options.UseSqlServer(cs);
         });
 
         // Provide ApplicationDbContext via the factory as TRANSIENT so Identity gets a fresh context per injection
         services.AddTransient(sp => sp.GetRequiredService<IDbContextFactory<ApplicationDbContext>>().CreateDbContext());
-
-        // Option 2 - Also register ApplicationDbContext as TRANSIENT for direct injection (if needed)
-        //services.AddDbContext<ApplicationDbContext>(options =>
-        //{
-        //    if (index == 3) options.UseNpgsql(cs);
-        //    // else if (index == 4) options.UseMySql(cs, ServerVersion.AutoDetect(cs));
-        //    else options.UseSqlServer(cs);
-        //}, contextLifetime: ServiceLifetime.Transient, optionsLifetime: ServiceLifetime.Singleton);
-
-
-
-
 
         // services.AddDbContextFactory<ApplicationDbContext>(options => options.UseSqlServer(connectionString), ServiceLifetime.Scoped);
         // services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
